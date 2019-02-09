4eca7a4fcda4b076beacff49cdaa2a1b8f8db370
Jeremy BOISSY <jboissy@e2r4p10.42.fr>
2019-02-09 15:46:08 +0100

    modifying the tileset

--- srcs/taag/menu_play.cpp
+++ srcs/taag/menu_play.cpp
@@ -27,8 +27,6 @@ void					menu_play(t_data data)
 
 	menu_choose_map(t_data(1, &path));
 
-	printf("path = %s\n", (MAP_PATH + path + MAP_EXT).c_str());
-
 	if (path != "")
 	{
 		t_game_engine	game = s_game_engine(MAP_PATH + path + MAP_EXT);
